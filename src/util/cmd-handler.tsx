20b9eaebe5e1bb5b8e0e91ef8815b15db1609ba8
thaag7734 <thaag7734@gmail.com>
2025-05-30 17:01:25 -0400

    make commands specify their usage

--- src/util/cmd-handler.tsx
+++ src/util/cmd-handler.tsx
@@ -5,37 +5,84 @@ type CommandResult = {
   clear?: boolean,
 }
 
-const COMMANDS = {
-  help: (): CommandResult => {
-    return {
-      output: (
-        <>
-          <span>Valid commands:</span><br/>
-          {(() => {
-            const cmdNames = Object.keys(COMMANDS).sort();
+type Command = {
+  fn: (args: string[]) => CommandResult,
+  usage: () => CommandResult,
+}
 
-            return (
-              <span>
-                {cmdNames.map((cmdName, idx) => (
-                    <span key={cmdName}>{cmdName}{
-                      (idx !== cmdNames.length - 1)
-                        ? ", "
-                        : ""
-                    }</span>
-                ))}
-                <br/>
-              </span>
-            );
-          })()}
-          <br/>
-          <span>Use <b>help &lt;command&gt;</b> to see detailed usage for a specific command.</span>
-          <br/><br/>
-        </>
-      ),
-    };
+const COMMANDS: Record<string, Command> = {
+  help: {
+    fn: (args) => {
+      if (args.length > 1) {
+        return {
+          output: (
+            <span>Too many arguments: command <b>help</b> accepts 0-1 arguments</span>
+          ),
+        };
+      } else if (args.length === 1) {
+        return COMMANDS[args[0]]?.usage() ?? {
+          output: (
+            <span>
+              <span>Invalid command: {args[0]}</span><br/><br/>
+              {COMMANDS["help"].fn([]).output}
+            </span>
+          ),
+        };
+      }
+
+      return {
+        output: (
+          <>
+            <span>Valid commands:</span><br/>
+            {(() => {
+              const cmdNames = Object.keys(COMMANDS).sort();
+
+              return (
+                <span>
+                  {cmdNames.map((cmdName, idx) => (
+                      <span key={cmdName}>{cmdName}{
+                        (idx !== cmdNames.length - 1)
+                          ? ", "
+                          : ""
+                      }</span>
+                  ))}
+                  <br/>
+                </span>
+              );
+            })()}
+            <br/>
+            <span>
+              Use <b>help &lt;command&gt;</b> to see detailed usage for a specific command.
+            </span>
+            <br/><br/>
+          </>
+        ),
+      };
+    },
+    usage: () => {
+      return {
+        output: (
+          <>
+            <span>Usage: help &lt;command&gt;</span>
+          </>
+        ),
+      }
+    },
   },
-  clear: (): CommandResult => {
-    return { output: null, clear: true }
+  clear: {
+    fn: () => {
+      return { output: null, clear: true }
+    },
+    usage: () => {
+      return {
+        output: (
+          <>
+            <span>Usage: clear</span>
+            <span>Clears the terminal</span>
+          </>
+        ),
+      };
+    },
   }
 }
 
@@ -49,12 +96,12 @@ export const processCommand = (cmd: string): CommandResult => {
   const tokens = trimmed.split(/\s+/);
   const cmdName = tokens[0];
 
-  return (COMMANDS[cmdName as keyof(typeof COMMANDS)] ?? (() => ({
+  return (COMMANDS[cmdName]?.fn ?? (() => ({
     output: (
       <span>
         <span>Invalid command: {cmdName}</span><br/><br/>
-        {COMMANDS["help"]().output}
+        {COMMANDS["help"].fn([]).output}
       </span>
     ),
-  })))()
+  })))(tokens.slice(1))
 }
