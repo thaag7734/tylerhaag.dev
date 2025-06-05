7094486adfec73dede002ca0a8ead89b0a902cf0
thaag7734 <thaag7734@gmail.com>
2025-06-05 18:25:22 -0400

    add ls command

--- src/util/commands/index.ts
+++ src/util/commands/index.ts
@@ -2,11 +2,13 @@ import { Command } from "../cmd-handler";
 import cat from "./cat";
 import clear from "./clear";
 import help from "./help";
+import ls from "./ls";
 
 const COMMANDS: Record<string, Command> = {
   help,
   clear,
   cat,
+  ls,
 }
 
 export default COMMANDS;
---
+++ src/util/commands/ls.tsx
@@ -0,0 +1,35 @@
+import { Command } from "../cmd-handler";
+import FILES from "../tfiles";
+
+const ls: Command = {
+  fn: (args) => {
+    return {
+      output: (
+        <>
+          <span role="heading">Available files:</span><br/>
+          {Object.keys(FILES).map((fileName) => (
+            <>
+              <span key={fileName}>{fileName}</span><br/>
+            </>
+          ))}
+          <br/>
+        </>
+      )
+    }
+  },
+  usage: () => {
+    return {
+      output: (
+        <>
+          <span>Usage: ls</span><br/>
+          <span>
+            Lists all files available for use by other commands.
+          </span><br/>
+          <br/>
+        </>
+      )
+    }
+  }
+};
+
+export default ls;
