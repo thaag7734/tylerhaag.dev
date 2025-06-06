863cf833301dcae861bc06ea5ef64f3372d3695d
thaag7734 <thaag7734@gmail.com>
2025-06-06 18:20:17 -0400

    add resume link as tfile

--- src/util/tfiles/index.ts
+++ src/util/tfiles/index.ts
@@ -2,11 +2,13 @@ import { ReactNode } from "react";
 import about from "./about";
 import contact from "./contact";
 import projects from "./projects";
+import resume from "./resume";
 
 const FILES: Record<string, ReactNode> = {
   about,
   contact,
   projects,
+  resume,
 }
 
 export default FILES;
---
+++ src/util/tfiles/resume.tsx
@@ -0,0 +1,12 @@
+export default (
+  <>
+    <span role="heading" className="bold">My Resume</span><br/>
+    <span>The link below will open in a new tab.</span><br/>
+    <a
+      href="https://static.tylerhaag.dev/Tyler-Haag-Resume.pdf"
+      target="_blank"
+      rel="noreferrer"
+    >https://static.tylerhaag.dev/Tyler-Haag-Resume.pdf</a><br/>
+    <br/>
+  </>
+)
